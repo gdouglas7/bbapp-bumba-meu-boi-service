36fb6c49bb883d8e34956f5446c25c1f1241cd97
George Nunes <george.nunes.trd@c6bank.com>
2020-07-07 22:20:28 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "bbapp-bumba-meu-boi-service"
---
+++ src/main/kotlin/bbapp/bumbameuboiservice/BbappBumbaMeuBoiServiceApplication.kt
@@ -0,0 +1,11 @@
+package bbapp.bumbameuboiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BbappBumbaMeuBoiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BbappBumbaMeuBoiServiceApplication>(*args)
+}
